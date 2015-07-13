e53900305dfb85e41b5655ddb17e1be1d3106a3f
Francesco Pontillo <francescopontillo@gmail.com>
2015-07-13 14:41:52 +0200

    feat: add Session class, check for valid session in user middleware

--- middleware/user.js
+++ middleware/user.js
@@ -1,6 +1,24 @@
 'use strict';
 
+var Session = require('./../model/Session');
+
 module.exports = function(req, res, next) {
-  req.user = 'frapontillo';
-  next();
+  var handle401 = function(res) {
+    res.status(401);
+    res.send('Resource protected, you need to register/authenticate first.');
+  };
+  var sessionToken = req.headers['session-key'] || req.query['session'];
+  if (sessionToken) {
+    Session.getUserWithSessionToken(sessionToken)
+      .then(function(user) {
+        req.user = user;
+        next();
+      })
+      .catch(function(err) {
+        console.error(err);
+        handle401(res);
+      });
+  } else {
+    handle401(res);
+  }
 };
--- model/Session.js
+++ model/Session.js
@@ -2,15 +2,30 @@
 
 var ParseClass = require('./ParseClass');
 var Parse = require('parse').Parse;
+var http = require('axios');
 
+var config = require('./../lib/config');
 var Session = ParseClass('_Session', ['sessionToken', 'user', 'expiresAt']);
 
-Session.exists = function(session) {
-  var query = Parse.Query(Session);
-  query.equalTo('sessionToken', session);
-  return query.first()
+Session.getUserWithSessionToken = function(sessionToken) {
+  return http({
+    url: 'https://api.parse.com/1/sessions',
+    method: 'GET',
+    headers: {
+      'X-Parse-Application-Id': config.getApplicationKey(),
+      'X-Parse-REST-API-Key': config.getRestKey(),
+      'X-Parse-Revocable-Session': 1,
+      'X-Parse-Session-Token': sessionToken
+    },
+    params: {
+      order: '-createdAt',
+      limit: 1,
+      select: 'user',
+      include: 'user'
+    }
+  })
     .then(function(session) {
-      return session ? true : false;
+      return session.data.results[0].user;
     });
 };
 
