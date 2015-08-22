2885a995a761eb77b406b793b7f0dcc952108af3
Francesco Pontillo <francescopontillo@gmail.com>
2015-08-22 13:47:20 +0200

    refactor(middleware/user): handle the secret token (`auth`) here
    Rename authorization query parameter from `session` to `auth`
    Rename authorization header from `session-key` to `auth-key`
    Only endpoints with the `user` middleware will have the secret set in `req.context`

--- middleware/user.js
+++ middleware/user.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = function(req, res, next) {
-  if (!req.config.secret) {
+  req.context.secret = req.query['auth'] || req.headers['auth-key'];
+  if (!req.context.secret) {
     res.status(401);
     res.send('Resource protected, you need to register/authenticate first.');
     return;
