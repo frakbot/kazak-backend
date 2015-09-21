38e0aa05f1cca1da8199ef368385eb3094c8bbc2
Francesco Pontillo <francescopontillo@gmail.com>
2015-09-21 09:30:10 +0200

    feat(endpoint/stars): increment/decrement stars on event
    Use old information and new one to understand what to increment/decrement
    Refactor Stars to Star (it's a class)
    Add stars property to Event

--- endpoint/stars.js
+++ endpoint/stars.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var buildStarsEndpoint = function(app) {
-  var Stars = require('./../model/Stars');
+  var _ = require('lodash');
+  var Q = require('q');
+  var Event = require('./../model/Event');
+  var Star = require('./../model/Star');
   var ErrorHandler = require('./../lib/firebaseErrorHandler');
 
   var buildPersonalSky = function(stars) {
@@ -14,8 +17,47 @@ var buildStarsEndpoint = function(app) {
   };
 
   var setForUid = function(req, res) {
-    var sky = buildPersonalSky(req.body);
-    Stars.put(req.context, req.params.uid, sky)
+    // use the admin secret to read the stars and update the events
+    var adminContext = _.clone(req.context);
+    adminContext.secret = app.locals.SUPER_SECRET;
+
+    var updateEvent = function(data, increment) {
+      var updates = [];
+      data.forEach(function(id) {
+        var promise = Event.get(req.context, id)
+          .then(function(event) {
+            // increment the number of stars, default to 0 if negative
+            var newStars = _.max([(event.stars || 0) + increment, 0]);
+            return Event.patch(adminContext, event.id, {
+              stars: newStars
+            })
+          });
+        updates.push(promise);
+      });
+      return Q.all(updates);
+    };
+
+    // read previous stars for uid
+    Star.get(adminContext, req.params.uid)
+      .then(function(originalSky) {
+        // map stars to an events id array
+        var pre = originalSky.events.map(function(e) {
+          return e.id;
+        });
+        var post = req.body;
+        // pre - post = unstarred
+        var unstarred = _.difference(pre, post);
+        // post - pre = starred
+        var starred = _.difference(post, pre);
+        return Q.all([starred, unstarred]);
+      })
+      .spread(function(starred, unstarred) {
+        return Q.all([updateEvent(starred, 1), updateEvent(unstarred, -1)]);
+      })
+      .then(function() {
+        var sky = buildPersonalSky(req.body);
+        return Star.put(req.context, req.params.uid, sky);
+      })
       .then(function(data) {
         res.send(data);
       })
